d2c18dc6f94d8d7a7d7b743a7997073ad05dab9d
Nkosilathi Vundla <nkosimarz@gmail.com>
2018-11-12 16:32:42 +0200

    api call and table up

---
+++ app.js
@@ -0,0 +1,57 @@
+var Greeter = /** @class */ (function () {
+    function Greeter(element) {
+        this.element = element;
+        // this.element.innerHTML += "The time is: ";
+        this.myTable = document.createElement('table');
+        this.element.appendChild(this.myTable);
+        // this.myTable.innerText = new Date().toUTCString();
+        //keep me 
+        // makes api call and returns data
+        function httpGet(theUrl) {
+            var xmlHttp = new XMLHttpRequest();
+            xmlHttp.open("GET", theUrl, false); // false for synchronous request
+            xmlHttp.send(null);
+            return xmlHttp.responseText;
+        }
+        //keep me 
+        // converts strings into array and table
+        function parseText(textString) {
+            // let outputArray = textString.replace(/'/g, '""');
+            var outputArray = JSON.parse(textString);
+            return outputArray;
+        }
+        // make calls and save the string data
+        var recordCount = httpGet("http://localhost:2050/recordCount");
+        var table_size = 40;
+        var columnsString = httpGet("http://localhost:2050/columns");
+        var recordsText = httpGet("http://localhost:2050/records?from=0&to=" + table_size);
+        var columnsArray = parseText(columnsString);
+        var recordsTable = parseText(recordsText);
+        // this.myTable.innerHTML += recordCount;
+        // this.myTable.innerHTML += columnsArray;
+        for (var _i = 0, recordsTable_1 = recordsTable; _i < recordsTable_1.length; _i++) {
+            var iEntry = recordsTable_1[_i];
+            var tr = document.createElement('tr');
+            this.myTable.appendChild(tr);
+            for (var _a = 0, iEntry_1 = iEntry; _a < iEntry_1.length; _a++) {
+                var jEntry = iEntry_1[_a];
+                var td = document.createElement('td');
+                td.appendChild(document.createTextNode(" " + jEntry));
+                tr.appendChild(td);
+            }
+        }
+    }
+    Greeter.prototype.start = function () {
+        // this.timerToken = setInterval(() => this.myTable.innerHTML = new Date().toUTCString(), 500);
+    };
+    Greeter.prototype.stop = function () {
+        clearTimeout(this.timerToken);
+    };
+    return Greeter;
+}());
+window.onload = function () {
+    var el = document.getElementById('content');
+    var greeter = new Greeter(el);
+    greeter.start();
+    // document.getElementById('MEH').innerHTML = "5";
+};
---
+++ app.ts
@@ -0,0 +1,73 @@
+﻿class Greeter {
+    element: HTMLElement;
+    myTable: HTMLElement;
+    timerToken: number;
+
+    constructor(element: HTMLElement) {
+        this.element = element;
+        // this.element.innerHTML += "The time is: ";
+        this.myTable = document.createElement('table');
+        this.element.appendChild(this.myTable);
+        // this.myTable.innerText = new Date().toUTCString();
+
+        //keep me 
+        // makes api call and returns data
+        function httpGet(theUrl) {
+            let xmlHttp = new XMLHttpRequest();
+            xmlHttp.open("GET", theUrl, false); // false for synchronous request
+            xmlHttp.send(null);
+            return xmlHttp.responseText;
+        }
+
+        //keep me 
+        // converts strings into array and table
+        function parseText(textString) {
+            // let outputArray = textString.replace(/'/g, '""');
+            let outputArray = JSON.parse(textString);
+
+            return outputArray;
+        }
+
+        // make calls and save the string data
+        const recordCount = httpGet("http://localhost:2050/recordCount");
+        let table_size = 40;
+        const columnsString = httpGet("http://localhost:2050/columns");
+        let recordsText = httpGet("http://localhost:2050/records?from=0&to=" + table_size);
+
+        const columnsArray = parseText(columnsString);
+        let recordsTable = parseText(recordsText);
+
+        // this.myTable.innerHTML += recordCount;
+        // this.myTable.innerHTML += columnsArray;
+
+        for (const iEntry of recordsTable) {
+
+            let tr = document.createElement('tr');
+            this.myTable.appendChild(tr);
+
+            for (const jEntry of iEntry) {
+
+                let td = document.createElement('td');
+                td.appendChild(document.createTextNode(" " + jEntry));
+                tr.appendChild(td);
+            }
+        }
+    }
+
+
+    start() {
+        // this.timerToken = setInterval(() => this.myTable.innerHTML = new Date().toUTCString(), 500);
+    }
+
+    stop() {
+        clearTimeout(this.timerToken);
+    }
+
+}
+
+window.onload = () => {
+    var el = document.getElementById('content');
+    var greeter = new Greeter(el);
+    greeter.start();
+    // document.getElementById('MEH').innerHTML = "5";
+};
