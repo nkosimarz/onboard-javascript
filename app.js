32e52543279ffa316f0068c6f2e613c01cd45d2c
Nkosilathi Vundla <nkosimarz@gmail.com>
2018-11-15 16:59:01 +0200

    some button functionality

--- app.js
+++ app.js
@@ -1,15 +1,13 @@
 var Greeter = /** @class */ (function () {
-    function Greeter(element) {
+    function Greeter(element, firstRecord) {
         this.element = element;
-        // this.element.innerHTML += "The time is: ";
         this.myTable = document.createElement('table');
         this.element.appendChild(this.myTable);
-        // this.myTable.innerText = new Date().toUTCString();
         // keep me, also rewrite
         // makes api call and returns data
         function getData(dataLink) {
             var apiCall = new XMLHttpRequest();
-            apiCall.open("GET", dataLink, false); // false to prevent asych
+            apiCall.open("GET", dataLink, false); // false to prevent asych // warning need to change
             apiCall.send(null);
             return apiCall.responseText;
         }
@@ -21,14 +19,16 @@ var Greeter = /** @class */ (function () {
             return outputArray;
         }
         // make calls and save the string data
-        var recordCount = getData("http://localhost:2050/recordCount");
-        var table_size = 50;
+        var recordCount = JSON.parse(getData("http://localhost:2050/recordCount"));
+        var tableSize = 500;
         var columnsString = getData("http://localhost:2050/columns");
-        var recordsText = getData("http://localhost:2050/records?from=0&to=" + table_size);
+        var endRecord = 1 * (firstRecord + tableSize);
+        // document.getElementById('content2').innerHTML = " " + endRecord; // this breaks code
+        var recordsText = getData("http://localhost:2050/records?from=" + firstRecord + "&to=" + tableSize);
         var columnsArray = parseText(columnsString);
         var recordsTable = parseText(recordsText);
-        // this.myTable.innerHTML += recordCount;
         // this.myTable.innerHTML += columnsArray;
+        // this.myTable.innerHTML += recordCount;
         for (var _i = 0, columnsArray_1 = columnsArray; _i < columnsArray_1.length; _i++) {
             var i = columnsArray_1[_i];
             var th = document.createElement('th');
@@ -39,33 +39,43 @@ var Greeter = /** @class */ (function () {
             var iEntry = recordsTable_1[_a];
             var tr = document.createElement('tr');
             this.myTable.appendChild(tr);
+            // this.myTable.insertBefore(tr, this.myTable.firstChild); // needed if i decide to use backward fill
             for (var _b = 0, iEntry_1 = iEntry; _b < iEntry_1.length; _b++) {
                 var jEntry = iEntry_1[_b];
                 var td = document.createElement('td');
                 td.appendChild(document.createTextNode(" " + jEntry));
                 tr.appendChild(td);
             }
+            // page is overfull stop filling and remove last child
+            if (this.myTable.offsetHeight >= window.innerHeight) {
+                this.myTable.removeChild(this.myTable.lastChild);
+                break;
+            }
         }
     }
-    Greeter.prototype.start = function () {
-        // this.timerToken = setInterval(() => this.myTable.innerHTML = new Date().toUTCString(), 500);
-    };
-    Greeter.prototype.stop = function () {
-        clearTimeout(this.timerToken);
-    };
     return Greeter;
 }());
-window.onload = function () {
+var lastEntry;
+function createPage(firstRecord) {
     var el = document.getElementById('content');
-    var greeter = new Greeter(el);
-    greeter.start();
-    // kill scroll bar
+    el.innerHTML = '';
+    var greeter = new Greeter(el, firstRecord);
+    lastEntry = el.lastChild.lastChild.firstChild.textContent;
+    // let el2 = document.getElementById('content2');
+}
+;
+function nextPage() {
+    createPage(parseInt(lastEntry));
+}
+window.onload = function () {
     document.documentElement.style.overflow = 'hidden';
-    // document.getElementById('MEH').innerHTML = "5";
+    createPage(0);
 };
-// pages
-// borders or highlights
-// buttons
-// difference in table dep on screensize full screen etc
-// tasks, pages to break, then add buttons next and prev, maybe pagination css
-// way to check for number of rows per page / if row has fulled page
+// create buttons and text box here instead of html
+// restructure functions and classes for better flow
+// prev and last page button
+// fill screen vs fill window
+// add data from api at any time
+// enter for text field
+// comments
+// fixed window percentage vs font size
--- app.ts
+++ app.ts
@@ -3,56 +3,55 @@
     myTable: HTMLElement;
     timerToken: number;
 
-    constructor(element: HTMLElement) {
+    constructor(element: HTMLElement, firstRecord) {
         this.element = element;
-        // this.element.innerHTML += "The time is: ";
         this.myTable = document.createElement('table');
         this.element.appendChild(this.myTable);
-        // this.myTable.innerText = new Date().toUTCString();
 
         // keep me, also rewrite
         // makes api call and returns data
-
-function getData(dataLink)
-{
-    let apiCall = new XMLHttpRequest();
-    apiCall.open("GET", dataLink, false); // false to prevent asych
-    apiCall.send(null);
-    return apiCall.responseText;
-}
+        function getData(dataLink: string) {
+            let apiCall = new XMLHttpRequest();
+            apiCall.open("GET", dataLink, false); // false to prevent asych // warning need to change
+            apiCall.send(null);
+            return apiCall.responseText;
+        }
 
         //keep me 
         // converts strings into array and table
         function parseText(textString) {
             // let outputArray = textString.replace(/'/g, '""');
             let outputArray = JSON.parse(textString);
-
             return outputArray;
         }
 
         // make calls and save the string data
-        const recordCount = getData("http://localhost:2050/recordCount");
-        let table_size = 50;
+        const recordCount = JSON.parse(getData("http://localhost:2050/recordCount"));
+        let tableSize = 500;
         const columnsString = getData("http://localhost:2050/columns");
-        let recordsText = getData("http://localhost:2050/records?from=0&to=" + table_size);
+        let endRecord = 1*(firstRecord + tableSize);
+        // document.getElementById('content2').innerHTML = " " + endRecord; // this breaks code
+        let recordsText = getData("http://localhost:2050/records?from=" + firstRecord + "&to=" + tableSize);
 
         const columnsArray = parseText(columnsString);
         let recordsTable = parseText(recordsText);
 
-        // this.myTable.innerHTML += recordCount;
         // this.myTable.innerHTML += columnsArray;
+        // this.myTable.innerHTML += recordCount;
 
-        for (const i of columnsArray){
+        for (const i of columnsArray) {
             let th = document.createElement('th');
             this.myTable.appendChild(th);
 
             th.appendChild(document.createTextNode(" " + i));
-       }
+        }
 
         for (const iEntry of recordsTable) {
 
             let tr = document.createElement('tr');
             this.myTable.appendChild(tr);
+
+            // this.myTable.insertBefore(tr, this.myTable.firstChild); // needed if i decide to use backward fill
 
             for (const jEntry of iEntry) {
 
@@ -60,35 +59,41 @@ function getData(dataLink)
                 td.appendChild(document.createTextNode(" " + jEntry));
                 tr.appendChild(td);
             }
+
+            // page is overfull stop filling and remove last child
+            if (this.myTable.offsetHeight >= window.innerHeight) {
+                this.myTable.removeChild(this.myTable.lastChild)
+                break;
+            }
         }
-    }
-
-
-    start() {
-        // this.timerToken = setInterval(() => this.myTable.innerHTML = new Date().toUTCString(), 500);
-    }
-
-    stop() {
-        clearTimeout(this.timerToken);
     }
 }
 
-window.onload = () => {
+let lastEntry;
+
+function createPage(firstRecord) {
+
     let el = document.getElementById('content');
-    let greeter = new Greeter(el);
-    greeter.start();
+    el.innerHTML = '';
+    let greeter = new Greeter(el, firstRecord);
+    lastEntry = el.lastChild.lastChild.firstChild.textContent;
+    // let el2 = document.getElementById('content2');
+};
 
-    // kill scroll bar
+function nextPage() {
+    createPage(parseInt(lastEntry));
+}
+
+window.onload = () => {
     document.documentElement.style.overflow = 'hidden';
-    // document.getElementById('MEH').innerHTML = "5";
+    createPage(0);
+}
 
-    };
-
-// pages
-// borders or highlights
-// buttons
-// difference in table dep on screensize full screen etc
-
-// tasks, pages to break, then add buttons next and prev, maybe pagination css
-
-// way to check for number of rows per page / if row has fulled page
+// create buttons and text box here instead of html
+// restructure functions and classes for better flow
+// prev and last page button
+// fill screen vs fill window
+// add data from api at any time
+// enter for text field
+// comments
+// fixed window percentage vs font size
