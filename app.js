11aa8d5cc9d361c333c031d6143622f9aa58c235
Nkosilathi Vundla <nkosimarz@gmail.com>
2018-11-19 15:22:12 +0200

    set row heights added more css, commenting and removing redundant code

--- app.js
+++ app.js
@@ -12,27 +12,38 @@ function parseText(textString) {
     return outputArray;
 }
 var recordCount = JSON.parse(getData("http://localhost:2050/recordCount"));
-var Greeter = /** @class */ (function () {
-    function Greeter(element, firstRecord) {
+var tableGenerator = /** @class */ (function () {
+    function tableGenerator(element, firstRecord) {
         this.element = element;
         this.myTable = document.createElement('table');
         this.element.appendChild(this.myTable);
-        // make calls and save the string data
-        var tableSize = 100;
-        var columnsString = getData("http://localhost:2050/columns");
-        var endRecord = (firstRecord + tableSize);
+        // get table height based on  remaining window size after buttons
+        var ctrlHeight = (document.getElementById('controls').offsetHeight);
+        var contentHeight = (window.innerHeight - document.getElementById('controls').offsetHeight);
+        this.myTable.setAttribute("height", "contentHeight");
+        // calculate row number based on table header + row heights + borders in CSS and ensure its never less than 1
+        var numRows = Math.floor((contentHeight - ctrlHeight - 26) / 46);
+        numRows = numRows ? 1 : numRows;
+        // set number of records to get from server and ensure that the last record doesnt exceed the limit.
+        var endRecord = (firstRecord + numRows - 1);
         endRecord = (endRecord >= recordCount ? recordCount - 1 : endRecord);
+        // get data from server
+        var columnsString = getData("http://localhost:2050/columns");
         var recordsText = getData("http://localhost:2050/records?from=" + firstRecord + "&to=" + endRecord);
+        // parse text from api call into arrays
         var columnsArray = parseText(columnsString);
         var recordsTable = parseText(recordsText);
         // this.myTable.innerHTML += columnsArray;
         // this.myTable.innerHTML += recordCount;
+        // alert(window.innerHeight - document.getElementById('controls').offsetHeight);
+        // write html for table header
         for (var _i = 0, columnsArray_1 = columnsArray; _i < columnsArray_1.length; _i++) {
             var i = columnsArray_1[_i];
             var th = document.createElement('th');
             this.myTable.appendChild(th);
             th.appendChild(document.createTextNode(" " + i));
         }
+        // write html for table
         for (var _a = 0, recordsTable_1 = recordsTable; _a < recordsTable_1.length; _a++) {
             var iEntry = recordsTable_1[_a];
             var tr = document.createElement('tr');
@@ -44,57 +55,95 @@ var Greeter = /** @class */ (function () {
                 td.appendChild(document.createTextNode(" " + jEntry));
                 tr.appendChild(td);
             }
-            // page is overfull stop filling and remove last child
-            if (this.myTable.offsetHeight > 0.95 * (window.innerHeight)) {
-                this.myTable.removeChild(this.myTable.lastChild);
-                break;
-            }
+            // get rid of this and use table height
+            // page is overfull stop filling and remove last child // 95% needs adajustment
+            // if (this.myTable.offsetHeight > (window.innerHeight - document.getElementById('controls').offsetHeight)) {
+            //     this.myTable.removeChild(this.myTable.lastChild)
+            //     // alert('overfull')
+            //     break;
+            // }
         }
+        // alert(this.myTable.offsetHeight);
+        // alert(window.innerHeight);
+        // alert((window.innerHeight - document.getElementById('controls').offsetHeight));
     }
-    return Greeter;
+    return tableGenerator;
 }());
-var lastEntry;
+var lastEntry, currentEntry;
 function createPage(firstRecord) {
     // alert(parseInt(firstRecord));
-    if (firstRecord >= recordCount || firstRecord < 0) {
-        alert('please select a NUMBER between 0 and ' + recordCount);
+    if (firstRecord >= recordCount || firstRecord < 0 || isNaN(firstRecord)) {
+        alert('please select a NUMBER between 0 and ' + (recordCount - 1));
         return;
     }
-    var el = document.getElementById('content');
-    el.innerHTML = '';
-    var greeter = new Greeter(el, parseInt(firstRecord)); // set number is slower
-    lastEntry = el.lastChild.lastChild.firstChild.textContent;
+    var contentsDiv = document.getElementById('content');
+    contentsDiv.innerHTML = '';
+    var tableElement = new tableGenerator(contentsDiv, parseInt(firstRecord)); // setting number is slower
+    lastEntry = contentsDiv.lastChild.lastChild.firstChild.textContent;
+    currentEntry = firstRecord;
 }
 ;
 function nextPage() {
-    createPage(lastEntry);
+    createPage(parseInt(lastEntry) + 1);
+}
+function prevPage() {
+    if ((lastEntry - 2 * (lastEntry - currentEntry) - 1) <= 0) {
+        createPage(0);
+    }
+    else {
+        createPage(currentEntry - (lastEntry - currentEntry) - 1);
+    }
+}
+function helpFunc() {
+    alert('Table for viewing records with IDs 0 to ' + (recordCount - 1) + '\nTo start from a specific record enter a record ID and click "Go To" \nMaximum number of viewable records is 100');
+}
+var resizeTimer;
+window.onresize = function () {
+    clearTimeout(resizeTimer);
+    resizeTimer = setTimeout(doneResizing, 500);
+};
+function doneResizing() {
+    createPage(currentEntry);
 }
 window.onload = function () {
     createPage(0);
+    var controlsDiv = document.getElementById('controls');
+    // disable browser scroll bar
     document.documentElement.style.overflow = 'hidden';
+    // html for buttons and input text fields
     var startButton = document.createElement('button');
-    startButton.appendChild(document.createTextNode('Start Page'));
-    document.getElementById('controls').appendChild(startButton);
+    startButton.appendChild(document.createTextNode('First Page'));
+    controlsDiv.appendChild(startButton);
     startButton.setAttribute("onclick", "createPage(0)");
+    var prevButton = document.createElement('button');
+    prevButton.appendChild(document.createTextNode('Prev'));
+    controlsDiv.appendChild(prevButton);
+    prevButton.setAttribute("onclick", "prevPage()");
     var nextButton = document.createElement('button');
-    nextButton.appendChild(document.createTextNode('Next Page'));
-    document.getElementById('controls').appendChild(nextButton);
+    nextButton.appendChild(document.createTextNode('Next'));
+    controlsDiv.appendChild(nextButton);
     nextButton.setAttribute("onclick", "nextPage()");
     var gotoButton = document.createElement('button');
     gotoButton.appendChild(document.createTextNode('Go To'));
-    document.getElementById('controls').appendChild(gotoButton);
+    controlsDiv.appendChild(gotoButton);
     gotoButton.setAttribute("onclick", 'createPage(document.getElementById("recordNum").value)');
     var inputField = document.createElement('input');
-    document.getElementById('controls').appendChild(inputField);
-    inputField.setAttribute("value", "starting record ...");
+    controlsDiv.appendChild(inputField);
+    inputField.setAttribute("value", "record...");
     inputField.setAttribute("id", "recordNum");
+    var helpButton = document.createElement('button');
+    helpButton.appendChild(document.createTextNode('Help'));
+    controlsDiv.appendChild(helpButton);
+    helpButton.setAttribute("onclick", "helpFunc()");
 };
+// ************************************************ NB ****************************************************
+// get tr size for pre calc table size
+// comments
+// ********************************************************************************************************
 // restructure functions and classes for better flow
-// prev and last page button
 // fill screen vs fill window or fixed window percentage vs font size
 // add data from api at any time (fecth data batch, set limits on data fetch, tenaries for limits, alerts)
 // enter for text field
 // comments
-// button space and sizing
+// button space and sizing, small window
 // type strict on numbers , firstrecord, lastentry recordCount
-// help button
--- app.ts
+++ app.ts
@@ -15,7 +15,7 @@ function parseText(textString) {
 
 const recordCount = JSON.parse(getData("http://localhost:2050/recordCount"));
 
-class Greeter {
+class tableGenerator {
     element: HTMLElement;
     myTable: HTMLElement;
 
@@ -24,20 +24,34 @@ class Greeter {
         this.myTable = document.createElement('table');
         this.element.appendChild(this.myTable);
 
-        // make calls and save the string data
+        // get table height based on  remaining window size after buttons
+        let ctrlHeight = (document.getElementById('controls').offsetHeight);
+        let contentHeight = (window.innerHeight - document.getElementById('controls').offsetHeight);
+        this.myTable.setAttribute("height", "contentHeight");
 
+        // calculate row number based on table header + row heights + borders in CSS and ensure its never less than 1
+        let numRows = Math.floor((contentHeight - ctrlHeight - 26) / 46);
+        numRows = numRows ? 1 : numRows;
+     
+        // set number of records to get from server and ensure that the last record doesnt exceed the limit.
+        let endRecord = (firstRecord + numRows - 1);
+        endRecord = (endRecord >= recordCount ? recordCount - 1 : endRecord);
 
-        let tableSize = 100;
+        // get data from server
         const columnsString = getData("http://localhost:2050/columns");
-        let endRecord = (firstRecord + tableSize);
-        endRecord = (endRecord >=recordCount ? recordCount-1 : endRecord);
         let recordsText = getData("http://localhost:2050/records?from=" + firstRecord + "&to=" + endRecord);
+
+        // parse text from api call into arrays
         const columnsArray = parseText(columnsString);
         let recordsTable = parseText(recordsText);
 
         // this.myTable.innerHTML += columnsArray;
         // this.myTable.innerHTML += recordCount;
 
+
+        // alert(window.innerHeight - document.getElementById('controls').offsetHeight);
+
+        // write html for table header
         for (const i of columnsArray) {
             let th = document.createElement('th');
             this.myTable.appendChild(th);
@@ -45,6 +59,7 @@ class Greeter {
             th.appendChild(document.createTextNode(" " + i));
         }
 
+        // write html for table
         for (const iEntry of recordsTable) {
 
             let tr = document.createElement('tr');
@@ -59,66 +74,121 @@ class Greeter {
                 tr.appendChild(td);
             }
 
-            // page is overfull stop filling and remove last child
-            if (this.myTable.offsetHeight > 0.95 * (window.innerHeight)) {
-                this.myTable.removeChild(this.myTable.lastChild)
-                break;
-            }
+            // get rid of this and use table height
+            // page is overfull stop filling and remove last child // 95% needs adajustment
+            // if (this.myTable.offsetHeight > (window.innerHeight - document.getElementById('controls').offsetHeight)) {
+            //     this.myTable.removeChild(this.myTable.lastChild)
+            //     // alert('overfull')
+            //     break;
+            // }
         }
+
+        // alert(this.myTable.offsetHeight);
+        // alert(window.innerHeight);
+        // alert((window.innerHeight - document.getElementById('controls').offsetHeight));
+
     }
 }
 
-let lastEntry;
+let lastEntry, currentEntry;
 
 function createPage(firstRecord) {
 
     // alert(parseInt(firstRecord));
-    if (firstRecord >= recordCount || firstRecord < 0) {
-        alert('please select a NUMBER between 0 and ' + recordCount);
+    if (firstRecord >= recordCount || firstRecord < 0 || isNaN(firstRecord)) {
+        alert('please select a NUMBER between 0 and ' + (recordCount - 1));
         return;
     }
 
-    let el = document.getElementById('content');
-    el.innerHTML = '';
-    let greeter = new Greeter(el, parseInt(firstRecord)); // set number is slower
-    lastEntry = el.lastChild.lastChild.firstChild.textContent;
+    let contentsDiv = document.getElementById('content');
+    contentsDiv.innerHTML = '';
+    let tableElement = new tableGenerator(contentsDiv, parseInt(firstRecord)); // setting number is slower
+    lastEntry = contentsDiv.lastChild.lastChild.firstChild.textContent;
+    currentEntry = firstRecord;
 };
 
 function nextPage() {
-    createPage(lastEntry);
+    createPage(parseInt(lastEntry) + 1);
+}
+
+function prevPage() {
+
+    if ((lastEntry - 2 * (lastEntry - currentEntry) - 1) <= 0) {
+        createPage(0);
+    }
+    else {
+        createPage(currentEntry - (lastEntry - currentEntry) - 1);
+    }
+}
+
+function helpFunc() {
+    alert('Table for viewing records with IDs 0 to ' + (recordCount - 1) + '\nTo start from a specific record enter a record ID and click "Go To" \nMaximum number of viewable records is 100')
+}
+
+
+let resizeTimer;
+
+window.onresize = function () {
+    clearTimeout(resizeTimer);
+    resizeTimer = setTimeout(doneResizing, 500);
+}
+
+function doneResizing() {
+    createPage(currentEntry);
 }
 
 window.onload = () => {
 
     createPage(0);
+    let controlsDiv = document.getElementById('controls');
+
+    // disable browser scroll bar
     document.documentElement.style.overflow = 'hidden';
+
+    // html for buttons and input text fields
     let startButton = document.createElement('button');
-    startButton.appendChild(document.createTextNode('Start Page'));
-    document.getElementById('controls').appendChild(startButton);
+    startButton.appendChild(document.createTextNode('First Page'));
+    controlsDiv.appendChild(startButton);
     startButton.setAttribute("onclick", "createPage(0)");
 
+    let prevButton = document.createElement('button');
+    prevButton.appendChild(document.createTextNode('Prev'));
+    controlsDiv.appendChild(prevButton);
+    prevButton.setAttribute("onclick", "prevPage()");
+
     let nextButton = document.createElement('button');
-    nextButton.appendChild(document.createTextNode('Next Page'));
-    document.getElementById('controls').appendChild(nextButton);
+    nextButton.appendChild(document.createTextNode('Next'));
+    controlsDiv.appendChild(nextButton);
     nextButton.setAttribute("onclick", "nextPage()");
 
     let gotoButton = document.createElement('button');
     gotoButton.appendChild(document.createTextNode('Go To'));
-    document.getElementById('controls').appendChild(gotoButton);
+    controlsDiv.appendChild(gotoButton);
     gotoButton.setAttribute("onclick", 'createPage(document.getElementById("recordNum").value)');
 
     let inputField = document.createElement('input');
-    document.getElementById('controls').appendChild(inputField);
-    inputField.setAttribute("value", "starting record ...");
+    controlsDiv.appendChild(inputField);
+    inputField.setAttribute("value", "record...");
     inputField.setAttribute("id", "recordNum");
+
+    let helpButton = document.createElement('button');
+    helpButton.appendChild(document.createTextNode('Help'));
+    controlsDiv.appendChild(helpButton);
+    helpButton.setAttribute("onclick", "helpFunc()");
 }
 
+
+
+// ************************************************ NB ****************************************************
+// get tr size for pre calc table size
+// comments
+
+// ********************************************************************************************************
 // restructure functions and classes for better flow
-// prev and last page button
 // fill screen vs fill window or fixed window percentage vs font size
 // add data from api at any time (fecth data batch, set limits on data fetch, tenaries for limits, alerts)
 // enter for text field
 // comments
-// button space and sizing
+// button space and sizing, small window
 // type strict on numbers , firstrecord, lastentry recordCount
-// help button
+
