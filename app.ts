6ede21ef9adaea8a14cccf1ba49218a4d6f38cbe
Nkosilathi Vundla <nkosimarz@gmail.com>
2018-11-26 14:24:23 +0200

    added asynch calls

--- app.ts
+++ app.ts
@@ -1,16 +1,75 @@
 ﻿// Onboarding Project - 2018
 
-const RECORD_COUNT: number = JSON.parse(getData("http://localhost:2050/recordCount"));
+let RECORD_COUNT: number; // total number of records available in webserver
 let resizeTimer; // delay required to reduce the number of calls made by browser when resizing the window
 let CONTROLS_HEIGHT;
-
+let CONTENT_HEIGHT;
+let columnsString; // variable to store columns text from api call
+let recordsText; // variable to store records from api call
+let columnsArray;
 
 // makes api call and returns data
-function getData(dataLink: string) {
+function getRecordCount() {
 	const apiCall = new XMLHttpRequest();
-	apiCall.open("GET", dataLink, false);
+
+	apiCall.onreadystatechange = function () {
+		if (apiCall.readyState === 4 && apiCall.status === 200) {
+			RECORD_COUNT = JSON.parse(apiCall.response);
+
+			if (columnsString !== undefined) {
+				tableElement.generateTable();
+			}
+		}
+	}
+	apiCall.open("GET", "http://localhost:2050/recordCount", true);
 	apiCall.send();
-	return apiCall.responseText;
+}
+
+function getCols() {
+	const apiCall = new XMLHttpRequest();
+	apiCall.onreadystatechange = function () {
+		if (apiCall.readyState === 4 && apiCall.status === 200) {
+			columnsString = apiCall.response;
+			columnsArray = parseText(columnsString);
+			if (RECORD_COUNT !== undefined) {
+				tableElement.generateTable();
+			}
+		}
+	}
+	apiCall.open("GET", "http://localhost:2050/columns", true);
+	apiCall.send();
+}
+
+function getTable(start: number, end: number, tableElement) {
+	const apiCall = new XMLHttpRequest();
+
+	const loadDiv = document.createElement('div');
+	loadDiv.setAttribute('class', 'loader')
+	tableElement.myTable.parentNode.appendChild(loadDiv);
+
+	apiCall.onreadystatechange = function () {
+		if (apiCall.readyState === 4 && apiCall.status === 200) {
+			tableElement.myTable.parentNode.removeChild(tableElement.myTable.parentNode.lastChild)
+			recordsText = apiCall.response;
+
+			const recordsTable = parseText(recordsText);
+
+			for (const iEntry of recordsTable) {
+
+				const tr = document.createElement('tr');
+				tableElement.myTable.appendChild(tr);
+
+				for (const jEntry of iEntry) {
+
+					const td = document.createElement('td');
+					td.appendChild(document.createTextNode(" " + jEntry));
+					tr.appendChild(td);
+				}
+			}
+		}
+	}
+	apiCall.open("GET", "http://localhost:2050/records?from=" + start + "&to=" + end, true);
+	apiCall.send();
 }
 
 // converts strings into array and table
@@ -30,7 +89,9 @@ class tableGenerator {
 		this.element = element;
 		this.firstRecord = firstRecord;
 		this.myTable = document.createElement('table');
+		this.element.appendChild(this.myTable);
 	}
+
 	generateTable() {
 
 		if (this.firstRecord >= RECORD_COUNT || this.firstRecord < 0) {
@@ -40,28 +101,20 @@ class tableGenerator {
 			alert('please select a number between 0 and ' + (RECORD_COUNT - 1));
 			return;
 		}
+
 		this.myTable.innerHTML = ''; // empty table div to fill in a new table
-		this.element.appendChild(this.myTable);
 
 		// get table height based on  remaining window size after buttons
-		const contentHeight = (window.innerHeight - CONTROLS_HEIGHT);
-		this.myTable.setAttribute("height", "contentHeight");
+		CONTENT_HEIGHT = (window.innerHeight - CONTROLS_HEIGHT);
+		this.myTable.setAttribute("height", "CONTENT_HEIGHT");
 
 		// calculate row number based on table header + row heights + borders in CSS and ensure its never less than 1
-		let numRows: number = Math.floor((contentHeight - CONTROLS_HEIGHT - 26) / 46);
+		let numRows: number = Math.floor((CONTENT_HEIGHT - CONTROLS_HEIGHT - 26) / 46);
 		numRows = numRows <= 0 ? 1 : numRows;
 
 		// set number of records to get from server and ensure that the last record doesnt exceed the limit.
 		this.endRecord = this.firstRecord - 1 + numRows;
 		this.endRecord = (this.endRecord >= RECORD_COUNT ? RECORD_COUNT - 1 : this.endRecord);
-
-		// get data from server
-		const columnsString: string = getData("http://localhost:2050/columns");
-		const recordsText: string = getData("http://localhost:2050/records?from=" + this.firstRecord + "&to=" + this.endRecord);
-
-		// parse text from api call into arrays
-		const columnsArray = parseText(columnsString);
-		const recordsTable = parseText(recordsText);
 
 		// write html for table header
 		for (const i of columnsArray) {
@@ -70,32 +123,21 @@ class tableGenerator {
 
 			th.appendChild(document.createTextNode(" " + i));
 		}
-
-		// write html for table
-		for (const iEntry of recordsTable) {
-
-			const tr = document.createElement('tr');
-			this.myTable.appendChild(tr);
-
-			for (const jEntry of iEntry) {
-
-				const td = document.createElement('td');
-				td.appendChild(document.createTextNode(" " + jEntry));
-				tr.appendChild(td);
-			}
-		}
+		getTable(this.firstRecord, this.endRecord, this);
 	}
 
 	// function used by the next page buttton, requires the last entry from previously generated table
 	nextPage() {
 
 		this.firstRecord = this.endRecord + 1;
-		// this.firstRecord = this.firstRecord > RECORD_COUNT - 1 ? RECORD_COUNT - 1 : this.firstRecord;
+		this.firstRecord = this.firstRecord > RECORD_COUNT - 1 ? RECORD_COUNT - 1 : this.firstRecord;
+		// getTable(this.firstRecord, this.endRecord, this);
 		this.generateTable();
 	}
 
 	firstPage() {
 		this.firstRecord = 0;
+		// getTable(this.firstRecord, this.endRecord, this);
 		this.generateTable();
 	}
 
@@ -103,7 +145,8 @@ class tableGenerator {
 	prevPage() {
 		// if the new first entry is less than zero, take user to the first page 
 		this.firstRecord = this.firstRecord - (this.endRecord - this.firstRecord) - 1;
-		// this.firstRecord = this.firstRecord < 0 ? 0 : this.firstRecord;
+		this.firstRecord = this.firstRecord < 0 ? 0 : this.firstRecord;
+		// getTable(this.firstRecord, this.endRecord, this);
 		this.generateTable();
 	}
 }
@@ -121,7 +164,7 @@ let customPage = () => {
 		alert('Please type in a number')
 		return;
 	}
-	tableElement.firstRecord
+	tableElement.firstRecord = inputVal;
 	tableElement.generateTable();
 }
 
@@ -140,6 +183,10 @@ let tableElement: tableGenerator;
 // load page for the first time and create buttons
 window.onload = () => {
 
+	// get columns and record coun,asynch
+	getRecordCount();
+	getCols();
+
 	const controlsDiv = this.controls;
 	CONTROLS_HEIGHT = controlsDiv.offsetHeight;
 
@@ -152,10 +199,13 @@ window.onload = () => {
 		<input value = "record..." id = "recordNum" >
 		<button onclick = "helpFunc()" > Help </button>`;
 
-	// initialise the table's parent div, create a tableElement and generate default table
+	// initialise the table's parent div, create a tableElement
 	contentsDiv2 = document.getElementById('content');
 	tableElement = new tableGenerator(contentsDiv2);
-	tableElement.generateTable();
 }
 
-
+// problems 
+// duplicate code
+// need to pre load columns, delete previous columns
+// need loading page
+// next button
